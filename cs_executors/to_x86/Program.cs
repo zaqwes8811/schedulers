04f24f01c9547bd3a2ba96ad77d64baa476a97e2
Your Name <you@example.com>
2016-06-15 12:15:56 -0700

    add .NET 3.5 executors

---
+++ cs_executors/to_x86/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Threading;
+
+class Executor : IDisposable
+{
+    private bool disposed;
+    private readonly Thread thread;
+    private Dispatcher dispatcher;
+    private Object thisLock = new Object();
+
+    public Executor()
+    {
+        thread = new Thread(ThreadMethod);
+        thread.Start();
+        while (true)
+        {
+            lock (thisLock)
+            {
+                if (dispatcher != null)
+                    break;
+            }
+        }
+        // object not escape scope while not init
+    }
+
+    public void Dispose()
+    {
+        if (disposed)
+            return;
+
+        dispatcher.BeginInvoke(
+            new Action(() => Dispatcher.CurrentDispatcher.InvokeShutdown()));
+        thread.Join();
+
+        disposed = true;
+    }
+
+    public void Post(Action del)
+    {
+        if (disposed)
+            return;
+
+        dispatcher.BeginInvoke(del);
+    }
+
+    private void ThreadMethod()
+    {
+        lock (thisLock)
+        {
+            dispatcher = Dispatcher.CurrentDispatcher;
+        }
+        Dispatcher.Run();
+    }
+}
+
+class Scope : IDisposable
+{
+    public Scope() 
+    {
+        if (SynchronizationContext.Current == null)
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+
+        context = SynchronizationContext.Current;
+    }
+
+    public static readonly Executor IO = new Executor();
+    public static readonly Executor FS = new Executor();
+    static SynchronizationContext context;
+
+    public static void UI_Post(Action del)
+    {
+        context.Post(s => del(), null);
+    }
+
+    public void Dispose()
+    {
+        IO.Dispose();
+        FS.Dispose();
+    }
+}
+
+class Program
+{
+    static void Main() 
+    {
+        using (Scope scope = new Scope()) { 
+            Scope.FS.Post(() => {
+                Scope.UI_Post(() => done = true);
+                Scope.IO.Post(() => 
+                {
+                    ioCall();
+                });
+            });
+
+            while (true) {
+                if (done) {
+                    break;
+                }
+            }
+        }  // dispose
+    }
+
+    static bool done = false;
+    static void ioCall()
+    {
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+    }
+}
